dd4af00287cf10f439a2f49831fda7e90b3e9ee8
M.Karthi Keyan <118340083+karthikeyanmani09@users.noreply.github.com>
2023-01-02 09:02:49 +0530

    initial

---
+++ src/components/KnowledgeGrid.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+    ></knowledge-element>
+  </ul>
+</template>
+<!--@select-topic="$emit('select-topic', $event)"-->
+<script>
+export default {
+  inject: ['topics'],
+};
+</script>
+//emits: ['select-topic']
